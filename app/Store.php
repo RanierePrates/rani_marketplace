c574c333e88fd22ad11952df62f477729d65229d
Raniere Prates <rsilva@uplexis.com.br>
2020-12-08 17:08:53 -0300

    criei as relações das tabelas

--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
