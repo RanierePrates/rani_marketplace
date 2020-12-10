533b2228746aa75d3391ac947ac1798700cb316b
Raniere Prates <raniere.prates@gmail.com>
2020-12-10 17:41:26 -0300

    criei barra de navegação

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,13 @@
 |
 */
 
+Route::get('/', 'HomeController@index')->name('home');
 
-
-
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
-
-    // Route::prefix('stores')->name('stores.')->group(function () {
-    //     Route::get('/', 'StoreController@index')->name('index');
-    //     Route::get('create', 'StoreController@create')->name('create');
-    //     Route::post('store', 'StoreController@store')->name('store');
-    //     Route::get('{store}/edit', 'StoreController@edit')->name('edit');
-    //     Route::post('update/{store}', 'StoreController@update')->name('update');
-    //     Route::get('destroy/{store}', 'StoreController@destroy')->name('destroy');
-    // });
-
+Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group(function () {
     Route::resource('stores', 'StoreController');
     Route::resource('products', 'ProductController');
 });
 
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
