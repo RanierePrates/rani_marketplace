bdfb8b10c33de4757a4d388c38840e868815d30e
Raniere Prates <raniere.prates@gmail.com>
2020-12-11 00:07:57 -0300

    ajuste para criar loja para o usuario logado

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -31,15 +31,6 @@
             <input class="form-control" type="text" name="slug">
         </div>
 
-        <div class="form-group">
-            <label for="">Usuário</label>
-            <select class="form-control" name="user" id="">
-                @foreach ($users as $user)
-                    <option value="{{ $user->id }}">{{ $user->name }}</option>
-                @endforeach
-            </select>
-        </div>
-
         <div>
             <button class="btn btn-lg btn-success" type="submit">Criar Loja</button>
         </div>
