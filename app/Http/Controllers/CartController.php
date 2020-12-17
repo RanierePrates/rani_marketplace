2573f62a3e1b30149ee5ad846fdec362566774c8
Raniere Prates <raniere.prates@gmail.com>
2020-12-17 07:37:09 -0300

    criei tela para exibir os produtos do carrinho

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,7 +8,8 @@ class CartController extends Controller
 {
     public function index()
     {
-        dd(session()->get('cart'));
+        $cart = session()->has('cart') ? session()->get('cart') : [];
+        return view('cart', compact('cart'));
     }
 
     public function add(Request $request)
@@ -26,5 +27,29 @@ class CartController extends Controller
         return redirect()->route('product.single', [
             'slug' => $product['slug']
         ]);
+    }
+
+    public function remove($slug)
+    {
+        if (!session()->has('cart')) {
+            return redirect()->route('cart.index');
+        }
+
+        $products = session()->get('cart');
+
+        $products = array_filter($products, function ($product) use ($slug) {
+            return $product['slug'] != $slug;
+        });
+
+        session()->put('cart', $products);
+        return redirect()->route('cart.index');
+    }
+
+    public function cancel()
+    {
+        session()->forget('cart');
+
+        flash('Compra cancelada')->success();
+        return redirect()->route('cart.index');
     }
 }
