3e2ae91843b3b51b04e7cf98c33e2c1c62d795ae
Raniere Prates <raniere.prates@gmail.com>
2020-12-27 10:51:34 -0300

    criei tela para exibir produtos por loja

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -3,7 +3,29 @@
 @section('content')
 
     <div class="row front">
-        @foreach ($products as $key => $product)
+
+        <div class="col-4">
+            @if ($store->logo)
+                <img src="{{ asset('storage/') . $store->logo }}" alt="Logo da loja" class="img-fluid">
+            @else
+                <img src="https://via.placeholder.com/600X300.png?text=logo" alt="Loja sem logo" class="img-fluid">
+            @endif
+        </div>
+        <div class="col-8">
+            <h2>{{ $store->name }}</h2>
+            <p>{{ $store->description }}</p>
+            <p>
+                <strong>Contatos:</strong>
+                <span>
+                    {{ $store->phone }} | {{ $store->mobile_phone }}
+                </span>
+            </p>
+        </div>
+        <div class="col-12">
+            <hr>
+            <h3 style="margin-bottom: 30px">Produtos desta loja </h3>
+        </div>
+        @forelse ($store->products as $key => $product)
             <div class="col-md-4">
                 <div class="card" style="width: 100%">
                     @if ($product->photos->count())
@@ -22,30 +44,10 @@
                 </div>
             </div>
             @if (($key + 1) % 3 == 0) </div><div class="row front"> @endif
-        @endforeach
-    </div>
-    <div class="row">
-
-        <div class="col-12">
-            <h2>Lojas Destaque</h2>
-            <hr>
-        </div>
-
-        @foreach ($stores as $store)
-            <div class="col-4">
-                @if ($store->logo)
-                    <img src="{{ asset('storage/') . $store->logo }}" alt="Logo da loja" class="img-fluid">
-                @else
-                    <img src="https://via.placeholder.com/600X300.png?text=logo" alt="Loja sem logo" class="img-fluid">
-                @endif
-                <h3>{{ $store->name }}</h3>
-                <p>{{ $store->description }}</p>
-
-                <a href="{{ route('store.single', ['slug' => $store->slug]) }}" class="btn btn-sm btn-success">
-                    Ver Loja
-                </a>
-            </div>
-        @endforeach
-
+        @empty
+            <h3 class="alert alert-warning">
+                Nenhum produto encontrado para a categoria loja
+            </h3>
+        @endforelse
     </div>
 @endsection
